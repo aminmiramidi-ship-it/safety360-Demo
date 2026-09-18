2b7c997b094d1c37ca099e57e3083f5dd9deb849
aminmiramidi-ship-it <amin.miramidi@gmail.com>
2026-09-18 23:54:49 +0200

    Mount interactive Safety360 Autopilot dock

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
+import AutopilotDock from "./AutopilotDock.jsx";
 import LanguageSwitcher from "./LanguageSwitcher.jsx";
 import "./i18n.js";
 import "./styles.css";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <LanguageSwitcher />
       <App />
+      <AutopilotDock />
     </BrowserRouter>
   </React.StrictMode>,
 );
