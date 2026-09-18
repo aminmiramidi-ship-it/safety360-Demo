51228f403a4c70adbb0680c4b3b6de7e72528dc6
aminmiramidi-ship-it <amin.miramidi@gmail.com>
2026-09-18 23:45:56 +0200

    Initialize i18n and expose language switcher

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
+import LanguageSwitcher from "./LanguageSwitcher.jsx";
+import "./i18n.js";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <LanguageSwitcher />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
